839ad3d1869b38cd4f409d235f1512cdc8b851e9
Tomás Beardsall <tomasagusbea@gmail.com>
2024-01-10 17:01:08 -0300

    favicon

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./pages/places.html','./src/index.html','./pages/contact.html','./pages/factory.html','./pages/us.html'],
+  content: ['./pages/places.html','./index.html','./pages/contact.html','./pages/factory.html','./pages/us.html'],
   theme: {
     extend: {
       fontFamily:{
